251de54811bfbf40f9ddcf8c28d20ed86295e11c
nasirshams1122 <nasirygo1122@gmail.com>
2026-02-13 16:11:31 +0500

    change name of icon

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import icon1 from '../assets/Navicon.svg'
-import icon2 from '../assets/star.svg'
+
+import star from '../assets/star.svg'
 import icon3 from '../assets/Navicon2.svg'
 import icon4 from '../assets/Navicon3.svg'
 import icon5 from '../assets/bell.svg'
@@ -28,7 +29,7 @@ const navBar = () => {
           <img src={icon1} alt="Icon 2" className="w-5 h-5 mr-5 " />
     </button>
     <button>
-          <img src={icon2} alt="Icon 1" className="w-5 h-5  mr-5 " />
+          <img src={star} alt="Icon 1" className="w-5 h-5  mr-5 " />
     </button>
   <button className="px-4 py-1 text-black rounded hover:bg-gray-200 text-sm font-Poppins">
       Dashboard
